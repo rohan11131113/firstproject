8429864f7c943d209c3b02eb40886f025e94bad8
Rohan <mangorerohan@gmail.com>
2025-07-14 18:40:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-print("hello from docker")
-app.run(host='0.0.0.0', port=5000)
+# app.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, I am Rohan – DevOps Intern Ready!"
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
